05fc09ec65d48a235782cc903fefcd1f35ea6b08
axis.1410 <adityasingh14102001@gmail.com>
2023-04-22 13:48:47 +0530

    Implemented connection with firebase collections

--- lib/location_dashboard.dart
+++ lib/location_dashboard.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, prefer_interpolation_to_compose_strings, avoid_print, no_leading_underscores_for_local_identifiers
 
 import 'dart:async';
 
@@ -8,8 +8,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_api_flutter_test/shared/constants.dart';
 import 'package:location/location.dart' as loc;
 import 'package:permission_handler/permission_handler.dart';
-
-import 'my_maps.dart';
 
 class LocationDashboard extends StatefulWidget {
   const LocationDashboard({super.key});
@@ -26,50 +24,75 @@ class _LocationDashboardState extends State<LocationDashboard> {
   void initState() {
     super.initState();
     _requestPermission();
-    location.changeSettings(interval: 500, accuracy: loc.LocationAccuracy.balanced);
+    location.changeSettings(interval: 500, accuracy: loc.LocationAccuracy.high);
     location.enableBackgroundMode(enable: true);
   }
 
   @override
   Widget build(BuildContext context) {
-    // Get the current user
-    final User? user = FirebaseAuth.instance.currentUser;
-    final String displayName = user?.displayName ?? 'Guest';
-
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+    final User? user = _auth.currentUser;
+    final String userName = user?.displayName ?? 'Anonymous';
     return Scaffold(
       backgroundColor: backgroundColor,
       appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              _auth.signOut();
+            },
+            icon: const Icon(Icons.logout),
+          )
+        ],
         backgroundColor: Colors.black,
-        title: Text('Signed in as $displayName'),
+        title: const Text('Live Location Tracker'),
       ),
       body: Column(
         children: [
           SizedBox(
             width: 200,
             child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                onPressed: () {
-                  _getLocation();
-                },
-                child: const Text('Add my location')),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                backgroundColor: Colors.black,
+              ),
+              onPressed: () {
+                _getLocation(userName);
+              },
+              child: const Text('Add my location'),
+            ),
           ),
           SizedBox(
             width: 200,
             child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                onPressed: () {
-                  _listenLocation();
-                },
-                child: const Text('Enable live location')),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                backgroundColor: Colors.black,
+              ),
+              onPressed: () {
+                _listenLocation(userName);
+              },
+              child: const Text('Enable live location'),
+            ),
           ),
           SizedBox(
             width: 200,
             child: ElevatedButton(
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
-                onPressed: () {
-                  _stopListening();
-                },
-                child: const Text('Stop live location')),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                backgroundColor: Colors.black,
+              ),
+              onPressed: () {
+                _stopListening();
+              },
+              child: const Text('Stop live location'),
+            ),
           ),
           Expanded(
             child: StreamBuilder(
@@ -82,29 +105,42 @@ class _LocationDashboardState extends State<LocationDashboard> {
                     itemCount: snapshot.data?.docs.length,
                     itemBuilder: (context, index) {
                       return ListTile(
-                        title: Text(snapshot.data!.docs[index]['name'].toString()),
+                        title: Text(
+                          snapshot.data!.docs[index]['name'].toString(),
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          ),
+                        ),
                         subtitle: Row(
                           children: [
-                            Text(snapshot.data!.docs[index]['latitude'].toString()),
+                            Text(
+                              snapshot.data!.docs[index]['latitude'].toString(),
+                              style: const TextStyle(
+                                  color: Colors.grey, fontWeight: FontWeight.w600, fontSize: 16),
+                            ),
                             const SizedBox(
                               width: 20,
                             ),
-                            Text(snapshot.data!.docs[index]['longitude'].toString()),
+                            Text(
+                              snapshot.data!.docs[index]['longitude'].toString(),
+                              style: const TextStyle(
+                                  color: Colors.grey, fontWeight: FontWeight.w600, fontSize: 16),
+                            ),
                           ],
-                        ),
-                        trailing: IconButton(
-                          icon: const Icon(Icons.directions),
-                          onPressed: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => MyMap(snapshot.data!.docs[index].id),
-                              ),
-                            );
-                          },
                         ),
                       );
                     });
               },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+            child: Align(
+              alignment: Alignment.bottomRight,
+              child: Text('Signed in as ${user?.displayName}',
+                  style: const TextStyle(color: Colors.grey)),
             ),
           ),
         ],
@@ -112,20 +148,33 @@ class _LocationDashboardState extends State<LocationDashboard> {
     );
   }
 
-  _getLocation() async {
+  _getLocation(userName) async {
     try {
       final loc.LocationData _locationResult = await location.getLocation();
-      await FirebaseFirestore.instance.collection('location').doc('user1').set({
-        'latitude': _locationResult.latitude,
-        'longitude': _locationResult.longitude,
-        'name': 'john'
-      }, SetOptions(merge: true));
+      final QuerySnapshot result = await FirebaseFirestore.instance
+          .collection('location')
+          .where('name', isEqualTo: userName)
+          .get();
+      if (result.docs.isNotEmpty) {
+        // User already exists in the location collection, update their location
+        await result.docs.first.reference.update({
+          'latitude': _locationResult.latitude,
+          'longitude': _locationResult.longitude,
+        });
+      } else {
+        // User does not exist in the location collection, create a new document
+        await FirebaseFirestore.instance.collection('location').add({
+          'latitude': _locationResult.latitude,
+          'longitude': _locationResult.longitude,
+          'name': userName
+        });
+      }
     } catch (e) {
       print(e);
     }
   }
 
-  Future<void> _listenLocation() async {
+  Future<void> _listenLocation(userName) async {
     _locationSubscription = location.onLocationChanged.handleError((onError) {
       print(onError);
       _locationSubscription?.cancel();
@@ -133,10 +182,14 @@ class _LocationDashboardState extends State<LocationDashboard> {
         _locationSubscription = null;
       });
     }).listen((loc.LocationData currentlocation) async {
-      await FirebaseFirestore.instance.collection('location').doc('user1').set({
+      print('long: ' +
+          currentlocation.longitude.toString() +
+          ' lat: ' +
+          currentlocation.latitude.toString());
+      await FirebaseFirestore.instance.collection('location').doc(userName).set({
         'latitude': currentlocation.latitude,
         'longitude': currentlocation.longitude,
-        'name': 'john'
+        'name': userName
       }, SetOptions(merge: true));
     });
   }
